037edd2da9f2d89f1eebf54a8b4a1bef1be500be
kasia m <kasia.napieracz@gmail.com>
2019-12-14 14:47:11 +0100

    program do testu grupowego

---
+++ grupowe.py
@@ -0,0 +1,11 @@
+def podnies(param):
+    return param.capitalize()
+
+def poloz(param):
+    return param.lower()
+
+def zamien(txt, co):
+    return txt.replace(co, naco)
+
+def policz(txt, co):
+    return txt.count(co)
