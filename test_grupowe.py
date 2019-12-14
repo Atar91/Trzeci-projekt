8b2cd42505abee681840bebcd4fc309a228826c5
Atar91 <taraga.agnieszka@gmail.com>
2019-12-14 13:39:41 +0100

    poprawiona wersja testu

--- test_grupowe.py
+++ test_grupowe.py
@@ -5,7 +5,7 @@ class test_grupowe(unittest.TestCase):
 
     def test_capitalize(self):
         txt = grupowe.podnies("ala ma kota")
-        self.assertEqual(txt, "Ala Ma Kota")
+        self.assertEqual(txt, "Ala ma kota")
 
 if __name__ == '__main__':
     unittest.main()
